a9e2ed593e8f0912dc4320a2218e85612aa21993
Leo Pop <leo32pop@gmail.com>
2017-10-21 20:55:20 +0300

    Made changes according to the style guides

--- designs.js
+++ designs.js
@@ -1,12 +1,11 @@
 const default_heigth = 10,
-      default_width = 10;
-
-let submit_button = $('#submit_button'),
-    input_height = $('#input_height'),
-    input_width = $('#input_width'),
-    input_color = $('#color_picker'),
-    input_show_grid = $('#show_grid_borders'),
-    grid = $('#pixel_canvas');
+    default_width = 10,
+    submit_button = $('#submit-button'),
+    input_height = $('#input-height'),
+    input_width = $('#input-width'),
+    input_color = $('#color-picker'),
+    input_show_grid = $('#show-grid-borders'),
+    grid = $('#pixel-canvas');
 
 let grid_height = input_height.val(),
     grid_width = input_width.val(),
@@ -16,10 +15,10 @@ function makeGrid(height, width) {
     grid.children().remove();
 
     for(let i = 0; i < height; i++) {
-        grid.append('<tr class="grid_row"></tr>');
-        let row = $('.grid_row').last();
+        grid.append('<tr class="grid-row"></tr>');
+        let row = $('.grid-row').last();
         for(let j = 0; j < width; j++) {
-            row.append('<td class="grid_cell"></td>');
+            row.append('<td class="grid-cell"></td>');
         }
     }
 }
@@ -32,7 +31,7 @@ submit_button.click(function ( event ) {
 input_height.on('change', function () { grid_height = $(this).val(); });
 input_width.on('change', function () { grid_width = $(this).val(); });
 input_color.on('change', function () { painting_color = $(this).val(); });
-input_show_grid.on('change', function () { $('.grid_cell').toggleClass('no_border'); });
+input_show_grid.on('change', function () { $('.grid-cell').toggleClass('no-border'); });
 
 grid.on('click', 'td', function ( event ) { $(event.target).css('background-color', painting_color); });
 
