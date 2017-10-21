4f56bb6c342eeb0238cbd94fd8bb6d15a14d11ad
Leo Pop <leo32pop@gmail.com>
2017-10-21 15:47:21 +0300

    Added basic functionality

--- designs.js
+++ designs.js
@@ -1,10 +1,33 @@
-// Select color input
-// Select size input
+let submit_button = $('#submit_button'),
+    input_height = $('#input_height'),
+    input_width = $('#input_width'),
+    input_color = $('#color_picker'),
+    grid = $('#pixel_canvas');
 
-// When size is submitted by the user, call makeGrid()
+let grid_height = input_height.val(),
+    grid_width = input_width.val(),
+    painting_color = input_color.val();
 
-function makeGrid() {
+function makeGrid(height, width) {
+    grid.children().remove();
 
-// Your code goes here!
-
+    for(let i = 0; i < height; i++) {
+        grid.append('<tr class="grid_row"></tr>');
+        let row = $('.grid_row').last();
+        for(let j = 0; j < width; j++) {
+            row.append('<td clas="grid_cell"></td>');
+        }
+    }
 }
+
+submit_button.click(function ( event ) {
+    event.preventDefault();
+    makeGrid(grid_height, grid_width)
+});
+
+input_height.on('change', function () { grid_height = $(this).val(); });
+input_width.on('change', function () { grid_width = $(this).val(); });
+input_color.on('change', function () { painting_color = $(this).val(); });
+
+
+grid.on('click', 'td', function ( event ) { $(event.target).css('background-color', painting_color); });
